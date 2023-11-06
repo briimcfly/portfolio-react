863445be99d4e421128c12563756e866560ce7b5
Bryan Bickel <briimcfly@gmail.com>
2023-11-05 21:09:42 -0800

    updated netlify form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,27 @@
-import Header from '../components/Header'
+import Header from '../components/Header';
 
-const title = "Contact Me"
+const title = "Contact Me";
 
 export default function ContactPage() {
   return (
-    <div className = "p-5">
-      <Header title = {title} />
+    <div className="p-5">
+      <Header title={title} />
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input type="text" name="name" className="form-control" id="name" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" name="email" className="form-control" id="email" />
+        </div>
+        <div className="form-floating">
+          <textarea className="form-control" placeholder="Leave a message here" id="message" name="message" style={{ height: '100px' }}></textarea>
+          <label htmlFor="message">Comments</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
